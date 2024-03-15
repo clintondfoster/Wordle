d3c234c157663206d9ccd40bbe8d77f0a628a2c0
clinton foster <clintondavidfoster@gmail.com>
2024-03-14 20:37:41 -0400

    updated GL styles

--- wordle-project/src/components/GL.js
+++ wordle-project/src/components/GL.js
@@ -9,62 +9,52 @@ function GL (props) {
     const game = useSelector(state => state.game);
     const { try_cur, guesses, answer } = game;
 
+    console.log("answer from GL", answer)
+    console.log("try_cur from GL", try_cur)
+    console.log("guesses from GL", guesses)
+
     //Check current guess 
     const currentGuess = guesses[props.gi];
     let style;
 
-    //Stores indices of the current letter in the answer, (identifies where the letter occurs)
     let indicesAnswer = [];
-   
     answer.forEach((char, idx) => {
         if (char === props.vl) {
             indicesAnswer.push(idx);
         }
     });
+    console.log("indicesAnswer from GL", indicesAnswer)
 
+
+    //Style logic for submitted guessess
     if (try_cur !== 0 && props.gi < try_cur) {
-        const isLetterInAnswer = answer.includes(props.vl);
-        const letterOccurrencesInGuess = currentGuess.filter(letter => letter === props.vl).length;
-        const letterOccurancesInAnswer = answer.filter(letter => letter === props.vl).length;
-        const isCorrectPosition = indicesAnswer.includes(props.idx);
-        const isLetterCorrect = currentGuess[props.idx] === props.vl;
-
-        //Determine the style based on the letter's correctness
-        if (isLetterInAnswer) {
-            if (isCorrectPosition && isLetterCorrect) {
-            style = StyleLabels.good;
-            } else if (!isCorrectPosition && letterOccurrencesInGuess <= letterOccurancesInAnswer && isLetterCorrect) {
-                style = StyleLabels.okay;
-            } else {
-                style = StyleLabels.bad;
+        if (answer.join("").includes(currentGuess[props.idx])) {
+            if (currentGuess.filter((x) => x === props.vl).length > 1) {
+                if (indicesAnswer.includes(props.idx)) {
+                    style = StyleLabels.good;
+                } else if (currentGuess.indexOf(props.idx) === props.idx && answer.indexOf(props.vl) !== currentGuess.indexOf(props.vl)) {
+                    style = StyleLabels.okay;
+                } else {
+                    style = StyleLabels.bad;
+                }
             }
-        } else {
-            style = StyleLabels.bad;
-        }
-}
+        } 
+    }
 
+// console.log("style", style)
 //Determines to flip the letter tile
 let flipped = try_cur !== 0 && props.gi < try_cur ? "flipped" : "";
 
-// return (
-//     <div className={`gl ${flipped}`}>
-//         <div className={`flipper d${props.idx}`}>
-//             <div className="front">{props.vl}</div>
-//             <div className="back" style={style}>{props.vl}</div>
-//         </div>
-//     </div>
-//     );
-// }
+
 return (
-    <div className={"gl " + flipped} >
+    <div className={"gl " + flipped}>
         <div className={"flipper d" + props.idx}>
-            <div className="front">{props.vl}</div>
+            <div className="front" style={style}>{props.vl}</div>
             <div className="back" style={style}>{props.vl}</div>
         </div>
     </div>
     )
 }
-
 
 export default GL;
 
